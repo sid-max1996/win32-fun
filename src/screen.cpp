e5ab287387aa5826b925665a534101b779398221
sid-max1996 <sid-max1996@yandex.ru>
2024-02-15 17:58:39 +0400

    v1.1.2   feat: improve PreventLockScreen

--- src/screen.cpp
+++ src/screen.cpp
@@ -11,5 +11,5 @@ void ScreenWakeUp () {
 }
 
 void PreventLockScreen() {
-  SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
+  SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
 }
