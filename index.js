df3aa0908f654b983c80141bf39a944bc5d9e17b
sid-max1996 <sid-max1996@yandex.ru>
2024-12-27 12:52:30 +0400

    v1.1.5 prebuilds

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const native = require('./build/Release/win32-fun.node')
+// const native = require('./build/Release/win32-fun.node')
+
+// Will load a compiled build if present or a prebuild.
+// If no build if found it will throw an exception
+const native = require("node-gyp-build")(__dirname);
 
 // getMemory and getMemorySync
 function check(path, callback) {
-  var result = undefined
-  var error = undefined
+  var result = undefined;
+  var error = undefined;
 
   try {
-    result = native.getDiskUsage(path)
+    result = native.getDiskUsage(path);
   } catch (error_) {
-    error = error_
+    error = error_;
   }
 
   if (callback) {
-    callback(error, result)
+    callback(error, result);
   }
 }
 
-exports.getDiskSpace = function(path, callback) {
+exports.getDiskSpace = function (path, callback) {
   if (callback) {
-    return check(path, callback)
+    return check(path, callback);
   }
 
   return new Promise((resolve, reject) => {
     check(path, (err, result) => {
-      err ? reject(err) : resolve(result)
-    })
-  })
-}
+      err ? reject(err) : resolve(result);
+    });
+  });
+};
 
 exports.getDiskSpaceSync = native.getDiskUsage;
 exports.turnOffMonitor = native.screenSleep;
